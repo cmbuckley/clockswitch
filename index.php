66fb1741be2cee27e98de3772b29c2b20b155aac
Chris Buckley <chris@cmbuckley.co.uk>
2020-04-15 01:05:13 +0100

    Update returning canonical

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="refresh" content="0;url=https://cmbuckley.co.uk/clockswitch/">
+    <link rel="canonical" href="https://cmbuckley.co.uk/clockswitch/" />
+    <title>clockswitch</title>
+</head>
+<body>
+    <p>This application has been returned to <a href="https://cmbuckley.co.uk/clockswitch">https://cmbuckley.co.uk/clockswitch/</a>.</p>
+</body>
+</html>
